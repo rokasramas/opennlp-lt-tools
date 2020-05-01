2846d8256616a0f3d76a8da6570ec8dcf4fc4f7a
Rokas Ramanauskas <rokasramas@gmail.com>
2020-05-01 15:00:28 +0300

    refactor: allow custom tokenizer model

--- src/main/java/opennlp/tools/tokenize/lang/lt/Tokenizer.java
+++ src/main/java/opennlp/tools/tokenize/lang/lt/Tokenizer.java
@@ -7,14 +7,18 @@ import opennlp.tools.tokenize.TokenizerME;
 import opennlp.tools.tokenize.TokenizerModel;
 
 public class Tokenizer extends TokenizerME {
-    private static String modelResourcePath = "/models/lt-token.bin";
+    private static final String defaultModelPath = "/models/lt-token.bin";
 
     public Tokenizer() throws IOException {
-        super(getModel());
+        super(getDefaultModel());
     }
 
-    private static TokenizerModel getModel() throws IOException {
-        InputStream is = Tokenizer.class.getResourceAsStream(modelResourcePath);
+    public Tokenizer(TokenizerModel model) {
+        super(model);
+    }
+
+    private static TokenizerModel getDefaultModel() throws IOException {
+        InputStream is = Tokenizer.class.getResourceAsStream(Tokenizer.defaultModelPath);
         TokenizerModel model = new TokenizerModel(is);
         is.close();
         return model;
