f0b28b5b603db24f3b2ea671edb73bee7d61f39a
Rokas Ramanauskas <rokasramas@gmail.com>
2020-05-01 15:27:09 +0300

    refactor: catch and log error when loading default tokenizer

--- src/main/java/opennlp/tools/tokenize/lang/lt/Tokenizer.java
+++ src/main/java/opennlp/tools/tokenize/lang/lt/Tokenizer.java
@@ -2,25 +2,31 @@ package opennlp.tools.tokenize.lang.lt;
 
 import java.io.InputStream;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import opennlp.tools.tokenize.TokenizerME;
 import opennlp.tools.tokenize.TokenizerModel;
 
 public class Tokenizer extends TokenizerME {
-    private static final String defaultModelPath = "/models/lt-token.bin";
+    private static final Logger LOGGER = Logger.getLogger(Tokenizer.class.getName());
+    private static final String DEFAULT_MODEL_PATH = "/models/lt-token.bin";
 
-    public Tokenizer() throws IOException {
-        super(getDefaultModel());
+    public Tokenizer() {
+        super(loadDefaultModel());
     }
 
     public Tokenizer(TokenizerModel model) {
         super(model);
     }
 
-    private static TokenizerModel getDefaultModel() throws IOException {
-        InputStream is = Tokenizer.class.getResourceAsStream(Tokenizer.defaultModelPath);
-        TokenizerModel model = new TokenizerModel(is);
-        is.close();
+    private static TokenizerModel loadDefaultModel() {
+        TokenizerModel model = null;
+        try (InputStream modelIn = Tokenizer.class.getResourceAsStream(DEFAULT_MODEL_PATH)) {
+            model = new TokenizerModel(modelIn);
+        } catch (NullPointerException | IOException e) {
+            LOGGER.log(Level.SEVERE, "Failed to load default tokenizer model: " + DEFAULT_MODEL_PATH, e);
+        }
         return model;
     }
 
